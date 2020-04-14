f3bb6f32a6cfb579521c3f707fc00cf06ba3024f
melindasterne <melindasterne@melindasternes-MacBook-Pro.local>
2020-04-14 14:07:22 -0700

    add program

---
+++ project1A.cpp
@@ -0,0 +1,151 @@
+class Fraction 
+{
+	private:
+		int numerator;
+		int denominator;
+	public:
+		void set(int, int);
+		int addedTo(Fraction) const;
+		int subtract(Fraction) const;
+		int multipliedBy(Fraction) const;
+		int dividedBy(Fraction) const;
+		bool isEqualTo(Fraction) const;
+};
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	Fraction f1;
+	Fraction f2;
+	Fraction result;
+
+	f1.set(9, 8);
+	f2.set(2, 3);
+	
+	cout << "\nArithmetic operations with fraction objects stored in the results class object\n";
+	cout << "------------------------------------------------------------------------------\n\n";
+
+	cout << "The product of ";
+	f1.print();
+	cout << " and ";
+	f2.print();
+	cout << " is ";
+	result = f1.multipliedBy(f2);
+	result.print();
+	cout << endl;
+
+	cout << "The quotient of ";
+	f1.print();
+	cout << " and ";
+	f2.print();
+	cout << " is ";
+	result = f1.dividedBy(f2);
+	result.print();
+	cout << endl;
+
+	cout << "The sum of ";
+	f1.print();
+	cout << " and ";
+	f2.print();
+	cout << " is ";
+	result = f1.addedTo(f2);
+	result.print();
+	cout << endl;
+
+	cout << "The difference of ";
+	f1.print();
+	cout << " and ";
+	f2.print();
+	cout << " is ";
+	result = f1.subtract(f2);
+	result.print();
+	cout << endl;
+
+	if(f1.isEqualTo(f2))
+	{
+		cout << "The two fractions are equal." << endl;
+	}
+	else
+	{
+		cout << "The two fractions are not equal." << endl;
+	}
+	
+	cout << "\n---------------------------------------------------------\n";
+	cout << "\nFraction class implementation test now successfully concluded\n";
+	
+
+	return 0;
+}
+
+
+void Fraction::set(int num, int den)
+//Post: numerator is set to num, denominator is set to den
+{
+	numerator = num;
+	denominator = den;
+
+}
+
+
+int Fraction::addedTo(Fraction fractionVar) const
+//Pre: Both arguments have been initialized
+//Post: sum of two arguments is returned
+{
+	Fraction sum;
+	sum.numerator = numerator + fractionVar.numerator;
+	sum.denominator = denominator + fractionVar.denominator;
+
+	return sum;
+}
+
+
+int Fraction::subtract(Fraction fractionVar) const
+//Pre: Both arguments have been initialized
+//Post: the result of one argument subtracted from the other is returned
+{
+	Fraction sub;
+	sub.numerator = numerator - fractionVar.numerator;
+	sub.denominator = denominator - fractionVar.denominator;
+
+	return sub;
+}
+
+
+int Fraction::multipliedBy(Fraction fractionVar) const
+//Pre: Both arguments have been initialized
+//Post: the product of two arguments is returned
+{
+	Fraction product;
+	product.numerator = numerator * fractionVar.numberator;
+	product.denominator = denominator * fractionVar.denominator;
+
+	return product;
+}
+
+
+int Fraction::dividedBy(Fraction fractionVar) const
+//Pre: Both arguments have been initialized
+//Post: sum of two arguments is returned
+{
+	Fraction quotient;
+	quotient.numerator = numerator + fractionVar.numberator;
+	quotient.denominator = denominator + fractionVar.denominator;
+
+	return quotient;
+}
+bool Fraction::isEqualTo(Fraction fractionVar1, Fraction FractionVar2) const
+//Pre: Both arguments have been initialized
+//Post: true is returned if the arguments are equal
+{
+	if(fractionVar1.numerator * fractionVar2.denominator == fractionVar1.denominator * fractionVar2.numerator)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
